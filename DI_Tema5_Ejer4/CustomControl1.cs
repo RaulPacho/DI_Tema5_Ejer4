568ec9842aa9d6dcca7a0328f52d1d827c728665
Raul Pacho <raulpachovivas@gmail.com>
2021-02-24 12:35:55 +0100

    Agregar archivos de proyecto.

---
+++ DI_Tema5_Ejer4/CustomControl1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_Tema5_Ejer4
+{
+    public partial class DibujoAhorcado : Control
+    {
+        int errores = 0;
+
+        public int Errores { 
+            get => errores;
+
+            set
+            {
+                errores = value;
+                CambiaError?.Invoke(this, EventArgs.Empty);
+                if(errores == 6)
+                {
+                    Ahorcado?.Invoke(this,EventArgs.Empty);
+                }
+            }
+        }
+
+        public DibujoAhorcado()
+        {
+            InitializeComponent();
+            CambiaError += refresca;
+
+        }
+
+
+        
+        [Category("Cositas")]
+        [Description("Pues eso, cuando cambia error... se activa")]
+        public event System.EventHandler CambiaError;
+
+        [Category("cositas")]
+        [Description("Cuando un jugador canta uno, no te jode")]
+        public event System.EventHandler Ahorcado;
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Pen p = new Pen(Color.Blue);
+            pe.Graphics.DrawLine(p,new Point(20, 400),new Point(20,100));
+            pe.Graphics.DrawLine(p, new Point(20, 100), new Point(100, 100));
+            pe.Graphics.DrawLine(p, new Point(100, 100), new Point(100, 120));
+            p.Color = Color.Red;
+            switch (Errores)
+            {
+                case 6:
+                    pe.Graphics.DrawLine(p, new Point(100, 250), new Point(145, 350));
+                    goto case 5;
+                case 5:
+                    pe.Graphics.DrawLine(p, new Point(100, 250), new Point(55, 350));
+                    goto case 4;
+                case 4:
+                    pe.Graphics.DrawLine(p, new Point(100, 160), new Point(100, 250));
+                    goto case 3;
+                case 3:
+                    pe.Graphics.DrawLine(p, new Point(100, 160), new Point(145, 200));
+                    goto case 2;
+                case 2:
+                    pe.Graphics.DrawLine(p, new Point(100, 160), new Point(55, 200));
+                    goto case 1;
+                case 1:
+                    pe.Graphics.DrawEllipse(p,new Rectangle(new Point(80,120),new Size(40,40)));
+                    break;
+                default:
+                    SolidBrush b = new SolidBrush(this.ForeColor);
+                    pe.Graphics.DrawString("¿Ande vas? ¡Payaso!", this.Font, b, 80, 120);
+                    break;
+            }
+
+        }
+
+        
+        public void refresca(Object sender,EventArgs e)
+        {
+            Refresh();
+        }
+        
+
+            
+    } 
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dibujoAhorcado1 = new DI_Tema5_Ejer4.DibujoAhorcado();
+            this.SuspendLayout();
+            // 
+            // dibujoAhorcado1
+            // 
+            this.dibujoAhorcado1.Errores = -1;
+            this.dibujoAhorcado1.Location = new System.Drawing.Point(107, 41);
+            this.dibujoAhorcado1.Name = "dibujoAhorcado1";
+            this.dibujoAhorcado1.Size = new System.Drawing.Size(347, 407);
+            this.dibujoAhorcado1.TabIndex = 0;
+            this.dibujoAhorcado1.Text = "dibujoAhorcado1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dibujoAhorcado1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DI_Tema5_Ejer4.DibujoAhorcado dibujoAhorcado1;
+    }
+}
+
